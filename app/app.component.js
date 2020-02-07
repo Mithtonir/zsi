5c1a5626763e576888ab318f8fc02f882c0f6f32
Toni <pawcio577@gmail.com>
2020-02-07 19:42:01 +0100

    logika update

--- app/app.component.js
+++ app/app.component.js
@@ -12,10 +12,12 @@ var AppComponent = /** @class */ (function () {
         this.value = 1;
         //values for fuzzy
         this.irrigation = new Array();
+        this.irrigation2 = new Array();
+        this.irrigation3 = new Array();
+        //end of fuzzy values
+        this.test = new Array();
     }
-    //end of fuzzy values
     AppComponent.prototype.ngOnInit = function () {
-        this.termLow();
     };
     AppComponent.prototype.startTimer = function () {
         var _this = this;
@@ -32,60 +34,81 @@ var AppComponent = /** @class */ (function () {
     };
     AppComponent.prototype.pauseTimer = function () {
         clearInterval(this.interval);
+        this.test = this.irrigation;
+        console.log('this.test', this.test);
+    };
+    AppComponent.prototype.jeb = function () {
+        this.termLow();
+        this.termAvg();
+        this.termHigh();
+        console.log('this.irrigation', this.irrigation);
+        console.log('this.irrigation2', this.irrigation2);
+        console.log('this.irrigation3', this.irrigation3);
     };
     AppComponent.prototype.termLow = function () {
         //1-45; 30-45
-        for (var i = 1; i <= 45; i++) {
+        for (var i = 0; i <= 45; i++) {
+            console.log('i', i);
             var x = i;
             var low;
             if (x > 30 && x <= 45) {
                 low = (x - 30) / (45 - 30);
             }
-            else if (x <= 30) {
+            if (x <= 30) {
                 low = 1;
             }
-            else if (x > 45) {
+            if (x > 45) {
                 low = 0;
             }
-            if (i === 45)
-                break;
-            this.irrigation.push(1, low);
+            this.irrigation.push(low);
         }
-        console.log('this.irrigation', this.irrigation);
+        // console.log('this.irrigation', this.irrigation)
     };
     AppComponent.prototype.termAvg = function () {
         //25 do 75, abv = 1 {40 do 60 }
         var i = 1;
+        var x1 = 40;
+        var x2 = 60;
+        var a = 25;
+        var b = 75;
         for (i; i <= 51; i++) {
-            var x = 25 + i;
+            console.log('i', i);
+            var x = 24 + i;
             var avg;
-            if (x >= 25 && x <= 75) {
-                avg = (x - 40) / (75 - 25);
+            if (a <= x && x <= x1) {
+                avg = (x - a) / (40 - 25);
             }
-            else if (x >= 40 && x <= 60) {
+            if (x1 <= x && x <= x2) {
                 avg = 1;
             }
-            else if (x >= 60 && x <= 75) {
+            if (x2 <= x && x <= b) {
+                avg = (b - x) / (b - x2);
+            }
+            else if (x < 25 && x > 75) {
                 avg = 0;
             }
+            this.irrigation2.push(avg);
         }
-        this.irrigation.push(2, avg);
         console.log('this.irrigation', this.irrigation);
     };
     AppComponent.prototype.termHigh = function () {
+        var a = 60;
+        var x1 = 80;
+        var x2 = 100;
         //60- 101; 70-80;
         for (var i = 1; i < 41; i++) {
-            var x = 60 + i;
-            if (x >= 60 && x < 101) {
-                var high = (x - 70) / (80 - 70);
-                this.irrigation.push(3, high);
+            var x = 59 + i;
+            var high;
+            if (a <= x && x <= x1) {
+                high = (x - a) / (x1 - a);
             }
-            else if (x >= 80 && x < 101) {
-                this.irrigation.push(3, 1);
+            if (x1 <= x && x <= x2) {
+                high = 1;
             }
-            else {
-                this.irrigation.push(3, 0);
+            else if (x < 60 && x > 100) {
+                high = 0;
             }
+            this.irrigation3.push(high);
         }
     };
     AppComponent = __decorate([
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,7 @@ import {
 import {
   MatSliderChange
 } from '@angular/material';
+import { Xliff2 } from '@angular/compiler';
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
@@ -18,10 +19,13 @@ export class AppComponent implements OnInit {
   //values for fuzzy
 
   irrigation = new Array();
+  irrigation2 = new Array();
+  irrigation3 = new Array();
 
   //end of fuzzy values
-
-  ngOnInit() {        this.termLow();
+test = new Array();
+  ngOnInit() {
+  
   }
   startTimer() {
     this.timeLeft = this.sliderValue;
@@ -29,7 +33,7 @@ export class AppComponent implements OnInit {
       if (this.timeLeft > 1) {
         this.timeLeft--;
         this.value++;
-      }
+              }
     }, 1000)
   }
   onInputChange(event: MatSliderChange) {
@@ -38,61 +42,86 @@ export class AppComponent implements OnInit {
 
   pauseTimer() {
     clearInterval(this.interval);
+    this.test = this.irrigation;
+    console.log('this.test', this.test)
     
+  }
+  jeb(){
+    this.termLow();
+    this.termAvg();
+    this.termHigh();
+    
+    console.log('this.irrigation', this.irrigation);
+    console.log('this.irrigation2', this.irrigation2);
+    console.log('this.irrigation3', this.irrigation3);
   }
 termLow() {
   //1-45; 30-45
-  for (var i = 1; i <= 45; i++) {
+  for (var i = 0; i <= 45; i++) {
+    console.log('i', i)
     var x = i;
     var low: number;
     if (x > 30 && x <= 45) {
       low = (x - 30) / (45 - 30);
-    }  else if (x <= 30) {
+    } if (x <= 30) {
       low = 1;
     }
-     else if (x>45){
+    if (x> 45) {
       low = 0;
     }
-    if(i === 45) break;
-    this.irrigation.push(1, low);
+    this.irrigation.push(low);
+    
   }
   
-  console.log('this.irrigation', this.irrigation)
+  // console.log('this.irrigation', this.irrigation)
   
 }
   termAvg() {
     //25 do 75, abv = 1 {40 do 60 }
     var i = 1;
+    var x1 = 40;
+    var x2 = 60;
+    var a = 25;
+    var b = 75;
     for (i; i <= 51; i++) {
-      var x = 25 + i;
+      console.log('i', i);
+      var x = 24 + i;
       var avg: number;
-      if (x >= 25 && x <= 75) {
-        avg = (x - 40) / (75 - 25);      
+      if (a <= x && x <= x1) {
+        avg = (x - a) / (40 - 25);      
       } 
-      else if (x >= 40 && x <= 60) {
+      if (x1<= x && x <=x2) {
         avg = 1;
-      } else if (x >= 60 && x <= 75) {
+      } 
+      if(x2<=x && x<= b){
+        avg = (b-x)/(b-x2);
+      }
+      else if (x<25 && x>75) {
         avg = 0
               }
+              this.irrigation2.push(avg);
     }
-    this.irrigation.push(2, avg);
     console.log('this.irrigation', this.irrigation)
   }
 
   termHigh() {
+    var a = 60;
+    var x1 = 80;
+    var x2 = 100;
     //60- 101; 70-80;
     for(var i=1; i<41; i++){
-      var x = 60 + i;
-      if(x>=60 && x<101){
-        var high = (x-70)/(80-70);
-        this.irrigation.push(3, high);
+      var x = 59 + i;
+      var high: number;
+      if(a<= x && x<= x1){
+        high = (x-a)/(x1-a);
+              }
+      if (x1<=x && x<= x2){
+        high = 1;
       }
-      else if (x>=80 && x< 101){
-        this.irrigation.push(3, 1)
+      else if (x<60 && x>100){
+        high = 0;
       }
-      else {
-        this.irrigation.push(3,0);
-      }
+      this.irrigation3.push(high);
     }
   }
 }
